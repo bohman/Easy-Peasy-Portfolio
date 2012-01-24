8c1140c007ebba28db7f9cb2f09ec6db45fe4056
Linus Bohman <linus@linusbohman.se>
2012-01-24 22:35:09 +0100

    Fixed spelling error on 404 page

--- 404.php
+++ 404.php
@@ -2,12 +2,10 @@
 // TODO: Make the 404 template more useful. And pretty. We like prettiness.
 get_header(); ?>
 
-  <ul class="posts center">
-      <li class="wysiwyg">
-        <h2>Oh. My. God!</h2>
-        <p>Something went wrong, so you get to see this error page! It's not particularly useful at the moment, but I promise to remedy that in future updates of this theme. It is, after all, only an alpha version.</p>
-        <p>For now, you'r more than welcome to <a href="<?php bloginfo('url'); ?>">head back to the start page</a>.</p>
-      </li>
-  </ul>
+  <div class="posts center wysiwyg">
+    <h2>Oh. My. God!</h2>
+    <p>Something went wrong, so you get to see this error page! It's not particularly useful at the moment, but I promise to remedy that in future updates of this theme. It is, after all, only an alpha version.</p>
+    <p>For now, you're more than welcome to <a href="<?php bloginfo('url'); ?>">head back to the start page</a>.</p>
+  </div>
 
 <?php get_footer(); ?>
