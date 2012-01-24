eb54c6374c5c804219bc169512b5452cab097060
Linus Bohman <linus@linusbohman.se>
2012-01-24 22:31:08 +0100

    Minor fixes: better .wysiwyg code style and hover event on gallery images

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-  <title><?php wp_title('&raquo; ',TRUE,'right'); ?><?php bloginfo('name'); ?></title>
+  <title><?php wp_title('&raquo; ', TRUE, 'right'); ?><?php bloginfo('name'); ?></title>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   <?php wp_head(); ?>
   <!--[if (gte IE 6)&(lte IE 8)]>
